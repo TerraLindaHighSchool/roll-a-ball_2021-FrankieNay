50f5e9f562b003a4b8fd509f0f8b9c2aa67862c8
FrankieNay <89489233+FrankieNay@users.noreply.github.com>
2022-01-10 15:49:53 -0800

    WIP (adding spice)

---
+++ Assets/CamSwitch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CamSwitch : MonoBehaviour
+{
+    public GameObject MainCamera;
+    public GameObject Camera2;
+
+   void Update ()
+    {
+        if (Input.GetButtonDown("1Key"))
+        { MainCamera.SetActive(true);
+          Camera2.SetActive(false);
+        }
+        if (Input.GetButtonDown("2Key"))
+        {
+            MainCamera.SetActive(false);
+            Camera2.SetActive(true);
+        }
+    }
+}
